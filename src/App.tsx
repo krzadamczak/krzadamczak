bbfaafde1ac8c48bbb479e269bc0c783baf787b9
Krzysztof Adamczak <krzadamczak@gmail.com>
2023-01-01 11:48:45 +0100

    Added routing and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Calculator from "./components/Calculator";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import LastEquations from "./components/LastCalculations";
+
 function App() {
-    return <div className='App'></div>;
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<LastEquations />} />
+                    <Route path='calculator' element={<Calculator />} />
+                </Route>
+            </Routes>
+        </HashRouter>
+    );
 }
 
 export default App;
